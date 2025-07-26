cf4f06c775b15f9df1e2b048b9683cd85ecdac64
mominsalarkhan <124016292+mominsalarkhan@users.noreply.github.com>
2025-07-26 16:54:18 -0400

    model.py

--- backend/model.py
+++ backend/model.py
@@ -1,5 +1,4 @@
 import re
-import joblib
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
@@ -10,8 +9,9 @@ texts = [
     "Pay electricity bill tomorrow",
     "Schedule team meeting next week",
     "Reply to urgent emails",
-    "Start coding assignment"
+    "Start coding assignment",
 ]
+
 labels = ["High", "Low", "Medium", "Low", "High", "Medium"]
 
 vectorizer = CountVectorizer()
@@ -20,6 +20,7 @@ X = vectorizer.fit_transform(texts)
 model = MultinomialNB()
 model.fit(X, labels)
 
+# Predict function
 def predict_priority(task):
     vec = vectorizer.transform([task])
     return model.predict(vec)[0]
